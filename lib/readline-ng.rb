743845e3c96f2e1483412f361127661b9a554f68
richo <richo@psych0tik.net>
2012-02-12 01:45:22 +1100

    Initial commit

---
+++ lib/readline-ng.rb
@@ -0,0 +1,37 @@
+module ReadlineNG
+
+  VERSION_MAJOR = 0
+  VERSION_MINOR = 0
+  VERSION_PATCH = 1
+  VERSION = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_PATCH}"
+
+  class Reader
+
+    # TODO Arrange for the terminal to be in raw mode etc.
+    #
+    attr_accessor :lines, :visible
+
+    def initialize
+      @buf = ""
+      @visible = true
+      @lines = []
+    end
+
+    def tick
+      t = STDIN.read_nonblock(128)
+      print_char(t) if @visible
+      @buf += t
+
+      raise Interrupt if @buf.include?("\x03")
+
+      a = @buf.split("\r")
+      @buf = @buf[-1] == "\r" ? "" : a.pop
+
+      @lines += a
+    rescue Errno::EGAIN
+      nil
+    end
+
+  end
+end
+
---
+++ readline-ng.gemspec
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+$:.push File.expand_path("../lib", __FILE__)
+require "readline-ng"
+
+Gem::Specification.new do |s|
+  s.name        = "readline-ng"
+  s.version     = ReadlineNG::VERSION
+  s.authors     = ["Rich Healey"]
+  s.email       = ["richo@psych0tik.net"]
+  s.homepage    = "http://github.com/richoH/readline-ng"
+  s.summary     = "Essentially, readline++"
+  s.description = s.summary
+
+  s.files         = `git ls-files`.split("\n")
+  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  s.require_paths = ["lib"]
+end
