f581c6fb465e051e1e2ade77901a9fd76e7edc89
richo <richo@psych0tik.net>
2012-02-12 02:29:12 +1100

    Notes and typo fix

--- lib/readline-ng.rb
+++ lib/readline-ng.rb
@@ -8,7 +8,10 @@ module ReadlineNG
   class Reader
 
     # TODO Arrange for the terminal to be in raw mode etc.
-    #
+    # XXX This probably needs to be a singleton, having more than one doesn't
+    # make a whole lot of sense, although potentially giving out rope is not a
+    # terrible idea here
+
     attr_accessor :lines, :visible
 
     def initialize
@@ -48,7 +51,7 @@ module ReadlineNG
       @buf = @buf[-1] == "\r" ? "" : a.pop
 
       @lines += a
-    rescue Errno::EGAIN
+    rescue Errno::EAGAIN
       nil
     end
 
