f704a28bb3c41b4aa104a96e922959b8334c4b27
richo <richo@psych0tik.net>
2012-08-18 17:33:36 +1000

    Release 0.1.0 since I'm feeling lucky

--- lib/readline-ng.rb
+++ lib/readline-ng.rb
@@ -7,8 +7,8 @@ module ReadlineNG
   n = ::ReadlineNG::ChunkedString.new
 
   VERSION_MAJOR = 0
-  VERSION_MINOR = 0
-  VERSION_PATCH = 9
+  VERSION_MINOR = 1
+  VERSION_PATCH = 0
   VERSION = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_PATCH}"
 
   CONTROL_BS  = "\x08"
